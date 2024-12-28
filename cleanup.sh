274774ea3d6d7540deb1cb549aa8ddb956915226
Kirill Menshov <k.menshov@gmail.com>
2024-12-28 21:32:28 +0300

    Use apt instead of apt-get

--- cleanup.sh
+++ cleanup.sh
@@ -4,10 +4,10 @@ green='\033[0;32m'
 reset_color='\033[0m'
 
 echo -e "${green}======= AUTOCLEAN =======${reset_color}"
-sudo apt-get autoclean
+sudo apt autoclean
 
 echo -e "${green}======= CLEAN =======${reset_color}"
-sudo apt-get clean
+sudo apt clean
 
 echo -e "${green}======= AUTOREMOVE =======${reset_color}"
-sudo apt-get -y autoremove
+sudo apt -y autoremove
--- update.sh
+++ update.sh
@@ -4,13 +4,13 @@ green='\033[0;32m'
 reset_color='\033[0m'
 
 echo -e "${green}======= UPDATE =======${reset_color}"
-sudo apt-get update
+sudo apt update
 
 echo -e "${green}======= UPGRADE =======${reset_color}"
-sudo apt-get -y upgrade
+sudo apt -y upgrade
 
 echo -e "${green}======= DIST-UPGRADE =======${reset_color}"
-sudo apt-get -y dist-upgrade
+sudo apt -y dist-upgrade
 
 echo -e "${green}======= AUTOREMOVE =======${reset_color}"
-sudo apt-get -y autoremove
+sudo apt -y autoremove
